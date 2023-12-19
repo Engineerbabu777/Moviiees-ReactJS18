f16f9d5478b82ccaa6023ffc7d300dcbbbcca0fd
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-19 19:09:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import { fetchDataFromApi } from './utils/api'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice'
+import { useEffect } from 'react'
+import Header from './components/Header/Header'
+
+function App () {
+  const dispatch = useDispatch()
+  const url = useSelector(state => state.home)
+  console.log(url)
+
+  useEffect(() => {
+    fetchApiConfig()
+  }, [])
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration').then(res => {
+      console.log(res)
+
+      const url = {
+        backdrop: res.images.secure_base_url + 'original',
+        poster: res.images.secure_base_url + 'original',
+        profile: res.images.secure_base_url + 'original'
+      }
+
+      dispatch(getApiConfiguration(url))
+    })
+  }
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+
+
+
+import React from 'react'
+import HeroBanner from './heroBanner/HeroBanner'
+
+function Home() {
+  return (
+    <div>
+        <HeroBanner />
+    </div>
+  )
+}
+
+export default Home
